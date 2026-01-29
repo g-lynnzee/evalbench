dba039c05631e4b31fb2255d71300435f1a5241d
Ismail Bouzouina <ismailmehdi@gmail.com>
2026-01-29 00:02:54 +0000

    feat: Add Cloud Run support and make the server port configurable via environment variables.

--- run_client.sh
+++ run_client.sh
@@ -1,2 +1,3 @@
+EVAL_CONFIG="datasets/bat/example_run_config.yaml"
 export PYTHONPATH=./evalbench:./evalbench/evalproto
-python3 evalbench/client/eval_client.py --experiment="evalbench/$EVAL_CONFIG"
+python3 evalbench/client/eval_client.py --experiment="evalbench/$EVAL_CONFIG" --endpoint="evaluator-zxwxgw5sma-uc.a.run.app"
