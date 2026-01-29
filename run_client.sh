52559a90884f4a0be2a0698ebc4b1497b17afac4
Ismail Bouzouina <ismailmehdi@gmail.com>
2026-01-29 18:11:27 +0000

    chore: configure local client execution with an insecure gRPC channel and temporarily disable evaluation calls.

--- run_client.sh
+++ run_client.sh
@@ -1,3 +1,2 @@
-EVAL_CONFIG="datasets/bat/example_run_config.yaml"
 export PYTHONPATH=./evalbench:./evalbench/evalproto
-python3 evalbench/client/eval_client.py --experiment="evalbench/$EVAL_CONFIG" --endpoint="evaluator-zxwxgw5sma-uc.a.run.app"
+python3 evalbench/client/eval_client.py --experiment="$EVAL_CONFIG" --endpoint="local"
