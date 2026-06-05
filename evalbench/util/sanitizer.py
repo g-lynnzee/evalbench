c7ad8cda0cc0cb838882c684bccd36e1c71d55d5
Erin Limbo <erinlimbo@google.com>
2026-06-05 21:31:10 +0000

    fix sanitzation error

--- evalbench/util/sanitizer.py
+++ evalbench/util/sanitizer.py
@@ -8,7 +8,8 @@ def sanitize_sql(sql: str, dialect: str = None):
         )  # required for gemini_1.0_pro, gemini_2.0_flash, gemini_2.5_pro
         .replace('sql: "', "")
         .replace("\\n", " ")
-        .replace("\\", "")
+        .replace("\\`", "`")
+        .replace('\\"', '"')
         .replace("  ", "")
         .replace("google_sql", "")
         .strip()
